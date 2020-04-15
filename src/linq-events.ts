ee1f9909239b681e5b0a749400fc73a7e6df2b7a
thomas <thomas.chiantia@gmail.com>
2020-04-15 10:31:58 -0400

    moved decorator test

--- src/linq-events.ts
+++ src/linq-events.ts
@@ -1,11 +1,9 @@
 import { Database, DeviceModel } from "./database/types";
 import { UtilRoutines } from "./common/types";
 import { LinqEventHandler } from "./linq/types";
-import { EventHandler, event } from "./linq/decorators";
 import { SYMBOLS } from "./ioc/constants.root";
 import { inject, injectable } from "inversify";
 
-@EventHandler()
 export class AppEventHandler implements LinqEventHandler {
   utils: UtilRoutines;
   database: Database<DeviceModel>;
