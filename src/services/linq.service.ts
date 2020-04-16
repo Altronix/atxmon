a680aff5d8b2fa83cf5faa4b58e385c6387ebeeb
thomas <thomas.chiantia@gmail.com>
2020-04-16 12:51:46 -0400

    file adds

---
+++ src/services/linq.service.ts
@@ -0,0 +1,53 @@
+import { AltronixLinqNetworkService, LinqNetworkService } from "./types";
+import { SYMBOLS } from "../ioc/constants.root";
+import { injectable, inject } from "inversify";
+
+@injectable()
+export class LinqService implements LinqNetworkService {
+  atx: AltronixLinqNetworkService;
+  constructor(
+    @inject(SYMBOLS.ATX_LINQ_SERVICE) atx: AltronixLinqNetworkService
+  ) {
+    this.atx = atx;
+  }
+
+  version(): string {
+    return this.atx.version();
+  }
+
+  listen(arg: string | number): LinqService {
+    this.atx.listen(arg);
+    return this;
+  }
+
+  connect(arg: string | number): LinqService {
+    this.atx.connect(arg);
+    return this;
+  }
+
+  close(arg: number): LinqService {
+    this.atx.close(arg);
+    return this;
+  }
+
+  send<T>(
+    serial: string,
+    meth: "GET" | "POST" | "DELETE",
+    path: string,
+    data?: T
+  ): Promise<any> {
+    return this.atx.send(serial, meth, path, data);
+  }
+
+  deviceCount(): number {
+    return this.atx.deviceCount();
+  }
+
+  nodeCount(): number {
+    return this.atx.nodeCount();
+  }
+
+  run(ms: number): Promise<any> {
+    return this.atx.run(ms);
+  }
+}
