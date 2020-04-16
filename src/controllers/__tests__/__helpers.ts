2f5b462aa4b2d8a2da03d18884bfac4933baea35
thomas <thomas.chiantia@gmail.com>
2020-04-16 12:58:13 -0400

    removed redundant test

--- src/controllers/__tests__/__helpers.ts
+++ src/controllers/__tests__/__helpers.ts
@@ -1,6 +1,6 @@
 import { UtilRoutines } from "../../common/types";
-import { DatabaseService, DatabaseConstructor } from "../../services/types";
-import { Repository } from "../../services/types";
+import { DatabaseService } from "../../services/types";
+import { Repository, LinqNetworkService } from "../../services/types";
 import { Controller, ControllerConstructor } from "../types";
 import { MockUtils } from "../../common/__test__/__mocks__/utils.mock";
 import { SYMBOLS } from "../../ioc/constants.root";
@@ -8,11 +8,13 @@ import { createContainer } from "../../ioc/container.root";
 import mockDatabaseContainers from "../../services/__tests__/__mocks__/containers.mock";
 
 export type MockedDatabase<M, E = M> = jest.Mocked<DatabaseService<M, E>>;
+export type MockedLinqNetworkService = jest.Mocked<LinqNetworkService>;
 
 // A test harness
 export interface Harness<Model, Entry = Model> {
-  database: jest.Mocked<DatabaseService<Model, Entry>>;
+  database: MockedDatabase<Model, Entry>;
   utils: MockUtils;
+  linq: MockedLinqNetworkService;
   controller: Controller<Model, Entry>;
 }
 
@@ -23,7 +25,8 @@ export async function setup<Model, Entry = Model>(
   let container = await createContainer();
   mockDatabaseContainers(container);
   let utils = container.get<MockUtils>(SYMBOLS.UTIL_ROUTINES); // TODO need mock
+  let linq = container.get<MockedLinqNetworkService>(SYMBOLS.LINQ_SERVICE);
   let database = container.get<MockedDatabase<Model, Entry>>(databaseSymbol);
   let controller = new controllerConstructor(utils, database);
-  return { database, utils, controller };
+  return { database, linq, utils, controller };
 }
