e719ac0a35fa1bd33d0264a88a059f44905051bc
thomas <thomas.chiantia@gmail.com>
2020-04-16 22:06:31 -0400

    graceful shutdown express

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ log("info", "starting app...");
   let app = await createApp();
   app.server.use(bodyParser.urlencoded({ extended: true }));
   app.server.use(bodyParser.json());
-  app.server.listen(3000);
+  let sock = app.server.listen(3000);
   let user = await app.services.users.create({
     name: "Thomas",
     pass: "Secret",
@@ -17,5 +17,6 @@ log("info", "starting app...");
 
   app.services.linq.listen(33455);
   app.services.linq.on("heartbeat", serial => log("info", serial));
-  app.services.linq.run(50);
+  await app.services.linq.run(50);
+  await sock.close();
 })();
