30ad8ea55385fb3aad1812f308c687fc474cf3a9
thomas <thomas.chiantia@gmail.com>
2020-04-20 21:52:25 -0400

    load config test

--- src/__test__/config.test.ts
+++ src/__test__/config.test.ts
@@ -9,7 +9,7 @@ test("Should load config", () => {
   expect(config.http.http).toBe(8080);
   expect(config.http.https).toBe(8443);
   expect(config.http.www).toBe("./www");
-  // expect(config.database.entities).toBe([__dirname + "/src/**/*.entity.ts"]);
+  expect(config.database.entities).toBe([__dirname + "/src/**/*.entity.ts"]);
   expect(config.database.name).toBe("./test.db");
   expect(config.database.type).toBe("sqlite");
   expect(config.linq.ipc).toEqual([]);
