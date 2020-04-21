75bd35ee40a06d064d57842cee8762b596f74eaf
thomas <thomas.chiantia@gmail.com>
2020-04-21 17:22:50 -0400

    proper passing of test args

--- scripts/test.js
+++ scripts/test.js
@@ -7,10 +7,9 @@ let path = require("path"),
 (async () => {
   try {
     let root = await utils.seekRoot(),
-      args = process.argv[2] ? ["-t", process.argv[2]] : [],
+      args = process.argv.slice(2),
       env = Object.assign({}, process.env, { ATXMON_PATH: root }),
       shell = process.platform === "win32" ? true : false;
-    if (process.argv[3]) args.push("--collect-coverage");
     p = await cp.spawn("jest", args, { stdio: "inherit", shell, env });
   } catch (e) {
     logger.error(e);
