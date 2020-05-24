6ac13b63a28afcae07f129ef3c003939ac39821e
thomas <thomas.chiantia@gmail.com>
2020-05-24 12:04:04 -0400

    validate query params

--- src/device/device.model.ts
+++ src/device/device.model.ts
@@ -1,3 +1,5 @@
+import { validate, validateSync, IsInt, IsString } from "class-validator";
+
 // DeviceModel
 export interface DeviceModel {
   serial: string;
@@ -8,6 +10,44 @@ export interface DeviceModel {
   web_version: string;
   mac: string;
   last_seen: number;
+}
+
+export class DeviceQuery implements DeviceModel {
+  @IsString()
+  serial!: string;
+
+  @IsString()
+  site_id!: string;
+
+  @IsString()
+  product!: string;
+
+  @IsString()
+  prj_version!: string;
+
+  @IsString()
+  atx_version!: string;
+
+  @IsString()
+  web_version!: string;
+
+  @IsString()
+  mac!: string;
+
+  @IsInt()
+  last_seen!: number;
+
+  static async valid(obj: any): Promise<DeviceModel> {
+    if (typeof obj === "string") obj = JSON.parse(obj);
+    let c = new DeviceQuery();
+    Object.assign(c, obj);
+    let ret = await validate(c, {
+      skipMissingProperties: true,
+      whitelist: true
+    });
+    if (ret.length) throw ret;
+    return c;
+  }
 }
 
 export type DeviceModelEntry = Omit<DeviceModel, "last_seen">;
